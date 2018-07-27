a380d4358dc2ac0ba5f4874168bedad9d0211668
Alex Fisher <fisherat@appstate.edu>
2018-07-27 11:00:45 -0400

    fixed errors again

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ Carrierwave.configure do |config|
   config.fog_credentials = {
     :provider => 'AWS',
     :aws_access_key_id => "#{ENV["AWS_ACCESS_KEY_ID"]}",
-    :aws_secret_access_id => "#{ENV['AWS_SECRET_ACCESS_KEY']}",
-    :region => "#{ENV['S3_REGION']}"
+    :aws_secret_access_key => "#{ENV['AWS_SECRET_ACCESS_KEY']}",
+    :region => "#{ENV['S3_REGION']}",
     :path_style => :true
   }
 
